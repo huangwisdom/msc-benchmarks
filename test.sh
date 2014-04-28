ed0fdbfbdfd49b5f68246a8606bad533a2ddf6cf
Richard Opalka <opalka.richard@gmail.com>
2014-04-28 14:14:57 +0200

    updating test script

--- test.sh
+++ test.sh
@@ -4,13 +4,12 @@ PROPS="$PROPS -Djboss.msc.benchmark.discrete.graph.services.count=1000"
 PROPS="$PROPS -Djboss.msc.benchmark.linear.graph.services.count=1001"
 PROPS="$PROPS -Djboss.msc.benchmark.complete.graph.services.count=1001"
 PROPS="$PROPS -DfailIfNoTests=false"
-#PROPS="$PROPS -Dtest=**/msc*/CycleDetectionBenchmarkTestCase#cycleDetectionDiscreteGraphAllServicesDown"
-#PROPS="$PROPS -Dtest=**/msc*/CycleDetectionBenchmarkTestCase#cycleDetectionLinearGraphAllServicesDown"
-#PROPS="$PROPS -Dtest=**/msc*/CycleDetectionBenchmarkTestCase#cycleDetectionCompleteGraphAllServicesDown"
+#PROPS="$PROPS -Dtest=**/msc*/CycleDetectionBenchmarkTestCase#discreteGraph"
+#PROPS="$PROPS -Dtest=**/msc*/CycleDetectionBenchmarkTestCase#linearGraph"
+#PROPS="$PROPS -Dtest=**/msc*/CycleDetectionBenchmarkTestCase#completeGraph"
+#PROPS="$PROPS -Dtest=**/msc*/ServiceStartBenchmarkTestCase#discreteGraph"
+#PROPS="$PROPS -Dtest=**/msc*/ServiceStartBenchmarkTestCase#linearGraph"
+#PROPS="$PROPS -Dtest=**/msc*/ServiceStartBenchmarkTestCase#completeGraph"
 
-for x in 0 1 2 3 4 5 6 7 8 9
-do
-  mvn clean install $PROPS 2>test$x.err | tee test$x.out
-done
+mvn clean install $PROPS
 
-exit 0
