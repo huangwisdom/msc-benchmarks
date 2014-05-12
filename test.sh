20626c88c0d631b0f5252784078c05c84b3af59d
Richard Opalka <opalka.richard@gmail.com>
2014-05-12 18:35:16 +0200

    yjp agent setup

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 PROPS="$PROPS -Djboss.msc.benchmark.discrete.graph.services.count=1000"
-PROPS="$PROPS -Djboss.msc.benchmark.linear.graph.services.count=1001"
-PROPS="$PROPS -Djboss.msc.benchmark.complete.graph.services.count=1001"
+PROPS="$PROPS -Djboss.msc.benchmark.linear.graph.services.count=1000"
+PROPS="$PROPS -Djboss.msc.benchmark.complete.graph.services.count=1000"
 PROPS="$PROPS -DfailIfNoTests=false"
+#export MAVEN_OPTS="$MAVEN_OPTS -agentpath:/home/ropalka/java/profilers/yjp-12.0.6/bin/linux-x86-64/libyjpagent.so"
 #PROPS="$PROPS -Dtest=**/msc*/CycleDetectionBenchmarkTestCase#discreteGraph"
 #PROPS="$PROPS -Dtest=**/msc*/CycleDetectionBenchmarkTestCase#linearGraph"
 #PROPS="$PROPS -Dtest=**/msc*/CycleDetectionBenchmarkTestCase#completeGraph"
