f227576adabc4e322ff60872312e444a5d4b7d4d
Richard Opalka <opalka.richard@gmail.com>
2014-10-27 08:15:48 +0100

    iterate through multiple installation threads

--- benchmark.sh
+++ benchmark.sh
@@ -4,36 +4,38 @@ PROPS="$PROPS -DfailIfNoTests=false -DenableAssertions=false"
 
 mvn clean install $PROPS
 
-OUT_DIR=benchmark-logs
-
 services=1000
-x=$services
-for i in 0 1 2 3 4
+for t in 1 2 3 4 5 6 7 8
 do
+  x=$services
+  OUT_DIR="benchmark-logs-$t"
   mkdir -p $OUT_DIR
-  for msc_version in msc1 msc2
+  for i in 0 1 2 3 4
   do
-    for suite_name in CycleDetectionBenchmarkTestCase ServiceStartBenchmarkTestCase
+    for msc_version in msc1 msc2
     do
-      for test_name in discreteGraph linearGraph completeGraph
+      for suite_name in CycleDetectionBenchmarkTestCase ServiceStartBenchmarkTestCase
       do
-        FILE="$msc_version.$suite_name.$test_name"
-        touch $OUT_DIR/$FILE
-        for iteration_no in 0 1 2 3 4 5 6 7 8 9
+        for test_name in discreteGraph linearGraph completeGraph
         do
-          PROPS="-DfailIfNoTests=false -DenableAssertions=false"
-          PROPS="$PROPS -Djboss.msc.benchmark.msc.threads.count=8"
-          PROPS="$PROPS -Djboss.msc.benchmark.installation.threads.count=8"
-          PROPS="$PROPS -Djboss.msc.benchmark.discrete.graph.services.count=$x"
-          PROPS="$PROPS -Djboss.msc.benchmark.linear.graph.services.count=$x"
-          PROPS="$PROPS -Djboss.msc.benchmark.complete.graph.services.count=$x"
-          PROPS="$PROPS -Dtest=**/$msc_version/$suite_name#$test_name"
-          mvn test $PROPS 2>$OUT_DIR/$FILE"_services"$x"_iteration"$iteration_no".err" | grep "execution time" | tee >> $OUT_DIR/$FILE".out"
+          FILE="$msc_version.$suite_name.$test_name"
+          touch $OUT_DIR/$FILE
+          for iteration_no in 0 1 2 3 4 5 6 7 8 9
+          do
+            PROPS="-DfailIfNoTests=false -DenableAssertions=false"
+            PROPS="$PROPS -Djboss.msc.benchmark.msc.threads.count=8"
+            PROPS="$PROPS -Djboss.msc.benchmark.installation.threads.count=$t"
+            PROPS="$PROPS -Djboss.msc.benchmark.discrete.graph.services.count=$x"
+            PROPS="$PROPS -Djboss.msc.benchmark.linear.graph.services.count=$x"
+            PROPS="$PROPS -Djboss.msc.benchmark.complete.graph.services.count=$x"
+            PROPS="$PROPS -Dtest=**/$msc_version/$suite_name#$test_name"
+            mvn test $PROPS 2>$OUT_DIR/$FILE"_services"$x"_iteration"$iteration_no".err" | grep "execution time" | tee >> $OUT_DIR/$FILE".out"
+          done
+          cat $OUT_DIR/$FILE.out | awk -F ' ' ' { print $8" "$10 } ' | tee $OUT_DIR/$FILE.report
         done
-        cat $OUT_DIR/$FILE.out | awk -F ' ' ' { print $8" "$10 } ' | tee $OUT_DIR/$FILE.report
       done
     done
+    # increment services count
+    x=$(($x + services))
   done
-  # increment services count
-  x=$(($x + services))
 done
